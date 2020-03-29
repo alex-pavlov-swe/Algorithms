a0aa3464282513e7ae5080bad5dfcbf01e48717f
PavlovAS <pavlov.cmc@gmail.com>
2020-03-28 19:17:40 -0700

    fibonacci iterative and recursive

---
+++ leetcode_python/509_fibonachi.py
@@ -0,0 +1,28 @@
+def fib(n):
+    if n == 0:
+        return n
+    if n == 1 or n == 2:
+        return 1
+    num1, num2 = 1, 1
+    newNum = 0
+    for i in range(2, n):
+        newNum = num1 + num2
+        num1 = num2
+        num2 = newNum
+    return num2
+
+
+def fibRec(n):
+    def helper(num1, num2, count):
+        if count == n:
+            return num1 + num2
+        else:
+            return helper(num2, num1 + num2, count + 1)
+    if n == 0:
+        return 0
+    if n == 1 or n == 2:
+        return 1
+    return helper(1, 1, 3)
+
+
+print(fibRec(10))
